97b46e1f38eff7954c5192213d72de2645357646
shahadrn <shahadrj77@gmail.com>
2021-11-06 01:08:22 +0300

    add new

---
+++ table.js
@@ -0,0 +1,34 @@
+// $(document).ready(function () {
+//     //Sample Json data
+//     var jsondata = [
+//         {
+//             "id": "1",
+//             "Name": "Name1",
+//             "Address": "Address1"
+//         },
+//         {
+//             "id": "2",
+//             "Name": "Name2",
+//             "Address": "Address2"
+//         },
+//         {
+//             "id": "3",
+//             "Name": "Name3",
+//             "Address": "Address3"
+//         },
+//         {
+//             "id": "4",
+//             "Name": "Name4",
+//             "Address": "Address4"
+//         },
+//         {
+//             "id": "5",
+//             "Name": "Name5",
+//             "Address": "Address5"
+//         }];
+
+//     $('#table').bootstrapTable({
+//         //Assigning data to table
+//         data: jsondata
+//     });
+// });
