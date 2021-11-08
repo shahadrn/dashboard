488e9db735166025ebaebdd7bb555f9ba3ceaab5
shahadrn <shahadrj77@gmail.com>
2021-11-08 04:33:01 +0300

    add new

--- js/products.js
+++ js/products.js
@@ -20,29 +20,29 @@ function renderHtml(prod) {
   let info = document.createElement('info');
 
   info.innerHTML = `
-    <div class="container my-3">
+    <div class="container my-3 w-75">
         <div class="card">
             <div class="card-body">
                 <div class="row">
-                    <div class="col-4">
+                    <div class="col-md-4 my-2">
                         <img src=${prod.image} class="img-fluid rounded-start" alt="...">
                     </div>
-                    <div class="col-md-5">
+                    <div class="col-md-5 my-2">
                         <div class="card-body">
                             <h5 class="card-title">${prod.name}</h5>
                             <p class="card-text">${prod.description}</p>
                         </div>
                     </div>
-                    <div class="col-md-3">
+                    <div class="col-md-3 my-2">
                         <p class="fs-4 font-weight-bolder">${prod.price}SAR</p>
                         <p class="text-success">${prod.status}</p>
                         <div class="row">
-                            <div class="col">
-                                <a href="#" class="btn btnDetails text-light bg-danger w-100">Details</a>
+                            <div class="col-md-8 my-2">
+                                <a href="#" class="btn btnDetails text-light w-100">Details</a>
                             </div>
                         </div>
                         <div class="row mt-3">
-                            <div class="col">
+                            <div class="col-md-8 my-2">
                                 <a href="#" class="btn text-light bg-danger w-100">Delete</a>
                             </div>
                         </div>
